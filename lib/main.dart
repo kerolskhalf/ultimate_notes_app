c142e18105741b0f1594b047348fd0dc714d6f32
Kerols <kerolskhalf9@gmail.com>
2025-09-18 05:38:16 +0300

    create home page & dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/home.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -8,6 +9,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomeView(),
+    );
   }
 }
