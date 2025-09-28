ee26c2a528f89194da15252e04a50db407b91145
Kerols <kerolskhalf9@gmail.com>
2025-09-28 17:30:44 +0300

    add colors to list and show colors in ui

--- lib/views/widgets/colors_list.dart
+++ lib/views/widgets/colors_list.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
-class ColorPicker extends StatelessWidget {
-  const ColorPicker({super.key});
-
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.isActive,required this.colorIndex});
+final bool isActive ;
+final Color colorIndex;
   @override
   Widget build(BuildContext context) {
     return
       Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 4),
-        child: const CircleAvatar(
-          backgroundColor:Colors.red ,
-          radius: 25,),
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: isActive
+            ? Container(
+            width: 54,
+            height: 54,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.grey.shade300, width: 2),
+            ),
+            child:  Padding(
+              padding: const EdgeInsets.all(3),
+              child: CircleAvatar(
+                    backgroundColor: colorIndex,
+                    radius: 25,
+                  ),
+            ))
+
+
+            : CircleAvatar(backgroundColor:colorIndex, radius: 25,)
       );
   }
 }
@@ -18,17 +34,48 @@ class ColorPicker extends StatelessWidget {
 
 
 
-class ColorsList extends StatelessWidget {
+class ColorsList extends StatefulWidget {
   const ColorsList({super.key});
 
+  @override
+  State<ColorsList> createState() => _ColorsListState();
+}
+
+class _ColorsListState extends State<ColorsList> {
+  int colorIndex = 0;
+   List<Color> kNotesColors = const [
+    Color(0xFFFFD700),
+    Color(0xFFFF6B6B),
+    Color(0xFF4ECDC4),
+    Color(0xFF45B7D1),
+    Color(0xFF96CEB4),
+    Color(0xFFFECE85),
+    Color(0xFFD63384),
+    Color(0xFF6F42C1),
+    Color(0xFFF39C12),
+    Color(0xFF20C997),
+    Color(0xFFE74C3C),
+    Color(0xFF3498DB),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height:50 ,
-      child: ListView.builder(itemBuilder: (context, index) {
-        return ColorPicker();
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+        return GestureDetector(
+           onTap: (){
+             colorIndex = index;
+             setState(() {});
+           },
+          child: ColorItem(
 
-      },itemCount: 7,
+            colorIndex:kNotesColors[index] ,
+            isActive: colorIndex == index,
+          ),
+        );
+
+      },itemCount: kNotesColors.length,
         scrollDirection: Axis.horizontal,
       ),
     );
